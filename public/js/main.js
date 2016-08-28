25f7758635095bcd83f4a2c9f795301b6f07f1f5
AshtonMeuser <ashtonmeuser@gmail.com>
2016-08-28 02:15:06 -0700

    Added bookmarklet generating DOM

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,9 @@
-// $('a').click(function (e) {
-//     // $(this).blur();
-//     e.preventDefault();
-// });
+onload = function () {
+   var input = document.getElementById('codepen_url');
+   input.oninput = generate_bookmarklet;
+   input.onpropertychange = input.oninput;
+};
+
+function generate_bookmarklet(event){
+  console.log(event.target.value);
+}
