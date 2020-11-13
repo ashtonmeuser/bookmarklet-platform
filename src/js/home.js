6af29275c4f011958fdd99da95bb8e41265ce0d8
Ashton Meuser <ashtonmeuser@gmail.com>
2020-11-12 21:07:40 -0800

    Convert index page to Alpine.js

--- src/js/home.js
+++ src/js/home.js
@@ -1,27 +1,15 @@
-const parseBookmarklet = (url) => {
-  const button = document.getElementById('anchor-generate-bookmarklet');
-  const codepenRegex = new RegExp('codepen.io/([-_a-z0-9]+)/pen/([-_a-z0-9]+).*', 'i');
-  const matches = url.match(codepenRegex);
-  if (matches !== null && matches[1] !== null && matches[2] !== null) {
-    button.classList.remove('disabled');
-    button.setAttribute('href', `/b/#${matches[1]}/${matches[2]}`);
-  } else {
-    button.classList.add('disabled');
-    button.removeAttribute('href');
-  }
-};
+const codepenRegex = new RegExp('codepen.io/([-_a-z0-9]+)/pen/([-_a-z0-9]+).*', 'i');
 
-const inputHandler = (event) => {
-  parseBookmarklet(event.target.value);
-};
-
-window.onload = () => {
-  const input = document.getElementById('input-codepen-url');
-  try {
-    input.oninput = inputHandler;
-    input.onpropertychange = input.oninput;
-    parseBookmarklet(input.value);
-  } catch (error) {
-    // Just ignore, I guess
-  }
-};
+window.data = () => ({
+  gistUrl: '',
+  get bookmarkletUrl() {
+    const matches = this.gistUrl.match(codepenRegex);
+    if (matches && matches[1] && matches[2]) {
+      return `/${matches[1]}/${matches[2]}`;
+    }
+    return null;
+  },
+  get valid() {
+    return this.bookmarkletUrl !== null;
+  },
+});
